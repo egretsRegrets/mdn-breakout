90c225980ef0af53b30eb2b6d7deb59c5a618ebc
mercantilism <samuel.galen.gardner@gmail.com>
2017-04-23 00:46:41 -0700

    input sensitive paddle

--- src/js/main.js
+++ src/js/main.js
@@ -5,10 +5,33 @@ var x = CANVAS.width / 2;
 var y = CANVAS.height - 30;
 var dx = 1.5; // x offset at each new frame
 var dy = -1.5; // y offset at each new frame
+var rightPressed = false;
+var leftPressed = false;
 
 var ball = {
     radius: 5,
-    color: '#177E89'
+    color: '#177E89',
+    draw: function () {
+        ctx.beginPath();
+        ctx.arc(x, y, ball.radius, 0, Math.PI * 2);
+        ctx.fillStyle = ball.color;
+        ctx.fill();
+        ctx.closePath();
+    }
+}
+
+var paddle = {
+    height: 10,
+    width: 75,
+    color: '#EF7030'
+};
+paddle.xPos = (CANVAS.width - paddle.width) / 2;
+paddle.draw = function(){
+    ctx.beginPath;
+    ctx.rect(paddle.xPos, CANVAS.height - paddle.height, paddle.width, paddle.height);
+    ctx.fillStyle = paddle.color;
+    ctx.fill();
+    ctx.closePath();
 }
 
 function newColor(){
@@ -16,19 +39,11 @@ function newColor(){
     return (palate.indexOf(ball.color) === palate.length - 1) ? palate[0] : palate[palate.indexOf(ball.color) + 1];
 }
 
-function drawBall() {
-    ctx.beginPath();
-    ctx.arc(x, y, ball.radius, 0, Math.PI * 2);
-    ctx.fillStyle = ball.color;
-    ctx.fill();
-    ctx.closePath();
-}
-
 function draw() {
     ctx.clearRect(0, 0, CANVAS.width, CANVAS.height); // clear the entire canvas before drawing
-    drawBall();
-    x += dx;
-    y += dy;
+    paddle.draw();
+    ball.draw();
+    
     if ( x + dx < ball.radius || x + dx > CANVAS.width - ball.radius){
         dx = -dx;
         ball.color = newColor();
@@ -37,5 +52,36 @@ function draw() {
         dy = -dy;
         ball.color = newColor();
     }
+
+    if(rightPressed && paddle.xPos < CANVAS.width - paddle.width){
+        paddle.xPos += 7;
+    }else if(leftPressed && paddle.xPos > 0){
+        paddle.xPos -= 7;
+    }
+
+    x += dx;
+    y += dy;
 }
+
+document.addEventListener('keydown', keyDownHandler, false);
+document.addEventListener('keyup', keyUpHandler, false);
+
+function keyDownHandler(event) {
+    if (event.keyCode === 37 || event.keyCode === 65){
+        leftPressed = true;
+    }
+    else if (event.keyCode === 39 || event.keyCode === 68){
+        rightPressed = true;
+    }
+}
+
+function keyUpHandler(event) {
+    if (event.keyCode === 37 || event.keyCode === 65){
+        leftPressed = false;
+    }
+    else if (event.keyCode === 39 || event.keyCode === 68){
+        rightPressed = false;
+    }
+}
+
 setInterval(draw, 10); // draw every 10miliseconds
